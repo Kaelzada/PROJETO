92e159751b80f6508621c7e13c1a298c6da34a93
Kael <carlos.machado.santos@escola.pr.gov.br>
2024-02-27 09:46:38 -0300

    exemplos aula 1 back end

---
+++ script.js
@@ -0,0 +1,27 @@
+// NUMBERS
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
+
+//STRINGS
+const cifrao = '/u0024'
+const aMaiusculo = '/u0041'
+const tique = '/u2705'
+const hiragana = '/u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+//BOOLEAN
+//true -> verdadeiro
+//false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+
